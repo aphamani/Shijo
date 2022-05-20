d53a425474c1d0015a9162891a6f2f97986d4646
DevShimi92 <39488884+DevShimi92@users.noreply.github.com>
2022-05-20 16:18:41 +0200

    Rework Check channel (live or not)

--- commands/twitch.js
+++ commands/twitch.js
@@ -1,14 +1,83 @@
-
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const log = require('log4js').getLogger("twitch");
-const twitchIsLive = require('../twitchModule')
+const log = require('log4js').getLogger('Twitch');
+const axios = require('axios');
+const TwitchJs = require('twitch-js').default;
+
+function getSettingsTwitch(client) {
+	log.trace('getSettingsTwitch');
+	client.twitch.set('clientId', process.env.clientIdTwitch);
+	client.twitch.set('clientSecret', process.env.clientSecret);
+	client.twitch.set('username', 'Shijo');
+}
+
+async function authenticationTwitch(client) {
+	log.trace('authenticationTwitch');
+	getSettingsTwitch(client);
+
+	const body = {
+		client_id: client.twitch.get('clientId'),
+		client_secret:  client.twitch.get('clientSecret'),
+		grant_type: 'client_credentials',
+	};
+
+	try {
+
+		const data = await axios('https://id.twitch.tv/oauth2/token', {
+			method: 'post',
+			headers: { 'Content-Type': 'application/json' },
+			data: JSON.stringify(body),
+		}).then(function(response) {
+			log.debug('Successful authentication on the api');
+			return response.data;
+		}).catch(function(error) {
+			if (error.response.status == 400) {
+				log.error('Authentication Failure');
+				return { access_token : null };
+			}
+		});
+
+		return data.access_token;
+	}
+	catch (error) {
+		log.error(error);
+	}
+}
+
+async function checkIsLiveTwitch(client) {
+	log.trace('checkIsLiveTwitch');
+
+	if (!client.twitch.get('access_token')) {
+		const access_token = await authenticationTwitch(client);
+		client.twitch.set('access_token', access_token);
+	}
+
+	const onAuthenticationFailure = () => new Promise((resolve) => {
+
+		const token = authenticationTwitch(client);
+
+		resolve(token);
+	});
+
+	const twitchJs = new TwitchJs({ token : client.twitch.get('access_token'), clientId : client.twitch.get('clientId'), onAuthenticationFailure });
+
+	twitchJs.api.get('streams', { search: { user_login: process.env.channelTwitchName } })
+		.then(response => {
+			if (response.data.length > 0) {
+				log.info('Chaine de ' + process.env.channelTwitchName + ' en live !');
+				client.user.setActivity(process.env.channelTwitchName, { name :response.data.title, type : 'STREAMING', url: 'https://www.twitch.tv/' + process.env.channelTwitchName });
+			}
+			else {
+				client.user.setPresence({ activities:[], status: 'online' });
+			}
+		});
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('twitch')
 		.setDescription('Check twitch account status (live or not)'),
-		async execute(interaction) {
-			twitchIsLive(interaction.client);
-			await interaction.reply('Status actualisé !');
+	async execute(interaction) {
+		checkIsLiveTwitch(interaction.client);
+		await interaction.reply('Status actualisé !');
 	},
 };
--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const { Client, Collection, Intents } = require('discord.js');
 const fs = require('node:fs');
 const log4js = require('log4js');
 log4js.configure('./config/log4js.json');
-const log = log4js.getLogger("main");
+const log = log4js.getLogger('Main');
 
 require('dotenv').config({ path: 'config/.env' });
 
 const shijo = new Client({
-    intents: [
-
-    ]
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES],
 });
+
+shijo.twitch = new Collection();
 
 shijo.commands = new Collection();
 
@@ -21,9 +21,8 @@ for (const file of commandFiles) {
 	shijo.commands.set(command.data.name, command);
 }
 
-
 shijo.on('ready', () => {
-    log.info('The bot is on');
+	log.info('The bot is on');
 });
 
 shijo.on('interactionCreate', async interaction => {
@@ -35,10 +34,27 @@ shijo.on('interactionCreate', async interaction => {
 
 	try {
 		await command.execute(interaction);
-	} catch (error) {
+	}
+	catch (error) {
 		log.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
+
+shijo.on('presenceUpdate', (oldPresence, newPresence) => {
+
+	if (!newPresence.activities) return false;
+	newPresence.activities.forEach(activity => {
+		if (activity.type == 'STREAMING' && newPresence.user.username == process.env.discordAccountTwitchName && newPresence.user.discriminator == process.env.discordAccountTwitchNumber) {
+			log.info(process.env.discordAccountTwitchName + 'in live !');
+			shijo.commands.execute('twitch');
+		}
+		else if (activity.type != 'STREAMING' && newPresence.user.username == process.env.discordAccountTwitchName && newPresence.user.discriminator == process.env.discordAccountTwitchNumber) {
+			log.info('End of ' + process.env.discordAccountTwitchName + 'live !');
+			shijo.commands.execute('twitch');
+		}
+	});
+});
+
 
 shijo.login(process.env.TOKEN);
